9c82932d0f04b094ca8f0543a7cc2c9bf2d5e021
Juampy NR <juampynr@gmail.com>
2020-03-14 13:28:36 +0100

    Initial commit

---
+++ web/core/modules/views/src/Plugin/views/argument/MonthDate.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\views\Plugin\views\argument;
+
+/**
+ * Argument handler for a month (MM)
+ *
+ * @ViewsArgument("date_month")
+ */
+class MonthDate extends Date {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $format = 'F';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $argFormat = 'm';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function summaryName($data) {
+    $month = str_pad($data->{$this->name_alias}, 2, '0', STR_PAD_LEFT);
+    return $this->dateFormatter->format(strtotime("2005" . $month . "15" . " 00:00:00 UTC"), 'custom', $this->format, 'UTC');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function title() {
+    $month = str_pad($this->argument, 2, '0', STR_PAD_LEFT);
+    return $this->dateFormatter->format(strtotime("2005" . $month . "15" . " 00:00:00 UTC"), 'custom', $this->format, 'UTC');
+  }
+
+  public function summaryArgument($data) {
+    // Make sure the argument contains leading zeroes.
+    return str_pad($data->{$this->base_alias}, 2, '0', STR_PAD_LEFT);
+  }
+
+}
---
+++ web/core/tests/Drupal/FunctionalTests/Installer/InstallerConfigDirectorySetNoDirectoryTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\FunctionalTests\Installer;
+
+use Drupal\Component\Utility\Crypt;
+
+/**
+ * Tests the installer when a custom config directory set up but does not exist.
+ *
+ * @group Installer
+ */
+class InstallerConfigDirectorySetNoDirectoryTest extends InstallerTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * The sync directory created during the install.
+   *
+   * @var string
+   */
+  protected $syncDirectory;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function prepareEnvironment() {
+    parent::prepareEnvironment();
+    $this->syncDirectory = $this->publicFilesDirectory . '/config_' . Crypt::randomBytesBase64() . '/sync';
+    $this->settings['settings']['config_sync_directory'] = (object) [
+      'value' => $this->syncDirectory,
+      'required' => TRUE,
+    ];
+  }
+
+  /**
+   * Verifies that installation succeeded.
+   */
+  public function testInstaller() {
+    $this->assertUrl('user/1');
+    $this->assertResponse(200);
+    $this->assertTrue(file_exists($this->syncDirectory) && is_dir($this->syncDirectory), "The directory {$this->syncDirectory} exists.");
+  }
+
+}
